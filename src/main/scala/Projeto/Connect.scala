98ebf5c4d93db2894383fbc29b43ba81fb9c6296
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-03-13 23:18:41 -0300

    * Modificando acesso de variáveis

--- src/main/scala/Projeto/Connect.scala
+++ src/main/scala/Projeto/Connect.scala
@@ -5,17 +5,13 @@ import java.sql.{Connection, DriverManager, ResultSet}
 import scala.concurrent.ExecutionContextExecutor
 case class Connect() {
   object SQLServerConnection {
-    // Configuração do servidor
     implicit val system: ActorSystem = ActorSystem("my-system")
-    //implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    // Configurações de conexão com o banco de dados SQL Server
-    val url = "jdbc:sqlserver://;servername=DESKTOP-G0TT3JA\\SQLEXPRESS;databaseName=DB"
-    val username = "hollemax"
-    val password = "cidadao"
+    private val url = "jdbc:sqlserver://;servername=DESKTOP-G0TT3JA\\SQLEXPRESS;databaseName=DB"
+    private val username = "hollemax"
+    private val password = "cidadao"
 
-    // Função para executar uma consulta SQL
     def executeQuery(sql: String): Seq[String] = {
       var connection: Connection = null
       var resultSet: ResultSet = null
@@ -28,17 +24,14 @@ case class Connect() {
         resultSet = statement.executeQuery(sql)
 
         while (resultSet.next()) {
-          // Aqui você pode acessar os resultados do ResultSet
           val id = resultSet.getInt("id")
           val Nome = resultSet.getString("Nome")
           val Descricao = resultSet.getString("Descricao")
-          // Adicione os dados ao resultado
           resultSeq :+= s"${id.toString} -  $Nome - $Descricao"
         }
       } catch {
         case e: Exception => e.printStackTrace()
       } finally {
-        // Fechar recursos
         if (resultSet != null) resultSet.close()
         if (connection != null) connection.close()
       }
